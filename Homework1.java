550863d5abdaeb7bb385edd8448ddadd12d7fdc6
Sneha Patel <Supersneha@patelsne-19-sp3.accounts.berkeleyprep.org>
2018-09-01 15:06:02 -0400

    Here is my homework!

--- Homework1.java
+++ Homework1.java
@@ -1,7 +1,7 @@
 /*
- * First and Last Names
+ * Sneha Patel
  */
-
+import javafx.application.Application;
 import java.awt.Rectangle;
 
 public class Homework1 {
@@ -9,9 +9,9 @@ public class Homework1 {
 	/* Fix the error(s) in this method so that it runs properly
 	 */
 	public static int problem1() {
-		int mystery = 1;
-		mystery = mystery + 1;
-		int mystery = 1 - 2 * mystery;
+		int mystery1 = 1;
+		int mystery2 = mystery1 + 1;
+		int mystery = 1 - 2 * mystery2;
 		return mystery;
 	}
 
@@ -19,24 +19,32 @@ public class Homework1 {
 	 * height of h
 	 */
 	public static Rectangle problem2(int w, int h) {
+		Rectangle box = new Rectangle(10,10,w, h);
+		return box;
 
 	}
 
 	/* Declare an integer, assign it a value of 5, then return it
 	 */
 	public static int problem3() {
+		int x = 5;
+		return x;
 
 	}
 
 	/* Declare and return a String containing the phrase "Computer Science"
 	 */
 	public static String problem4() {
+		String coolclass= "Computer Science";
+		return coolclass;
 
 	}
 
 	/* Add 5 to num and return the new value
 	 */
 	public static int problem5(int num) {
+		int answer= num + 5;
+		return answer;
 
 	}
 
